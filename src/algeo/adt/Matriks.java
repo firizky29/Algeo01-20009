9dfa4ecc22099b6fc6fd7cc692def0cadf2a2432
Ahmad Romy <ahmadromyzahran@gmail.com>
2021-09-23 09:18:19 +0700

    Buat bacatulis Matriks ke file dan command line

--- src/algeo/adt/Matriks.java
+++ src/algeo/adt/Matriks.java
@@ -1,4 +1,85 @@
 package algeo.adt;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.Scanner;
+
 // disini tulis adt matriksnya ya guys
 public class Matriks {
+    public static int[][] bacaMatriks1(String filePath) throws FileNotFoundException {
+        int baris,kolom;
+        int i,j;
+
+        Scanner in = new Scanner(new File(filePath));
+        baris = in.nextInt();kolom = in.nextInt();
+        int[][] m = new int[baris][kolom];
+        for(i=0;i<baris;i++) {
+            for(j=0;j<kolom;j++) {
+                m[i][j] = in.nextInt();
+            }
+        }
+        in.close();
+        return m;
+    }
+
+    public static int[][] bacaMatriks2(){
+        int baris,kolom;
+        int i,j;
+
+        Scanner in = new Scanner(System.in);
+        baris = in.nextInt();kolom = in.nextInt();
+        int[][] m = new int[baris][kolom];
+        for(i=0;i<baris;i++) {
+            for(j=0;j<kolom;j++) {
+                m[i][j] = in.nextInt();
+            }
+        }
+        in.close();
+        return m;
+    }
+    public static void tulisMatriks1(int[][] m, String filePath) {
+        // matriks tidak kosong
+        int baris=m.length, kolom=m[0].length;
+        int i,j;
+
+        File file = new File(filePath);
+        try{
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            Formatter out = new Formatter(file.getAbsoluteFile());
+            for(i=0;i<baris;i++) {
+                for(j=0;j<kolom;j++) {
+                    if(j==kolom-1) {
+                        out.format("%d%n", m[i][j]);
+                    } else {
+                        out.format("%d ", m[i][j]);
+                    }
+                }
+            }
+            out.close();
+        } catch(IOException ex) {
+            System.exit(1);
+        }
+    }
+
+    public static void tulisMatriks2(int[][] m){
+        // matriks tidak kosong
+        int baris=m.length, kolom=m[0].length;
+        int i,j;
+
+        Formatter out = new Formatter(System.out);
+        for(i=0;i<baris;i++) {
+            for(j=0;j<kolom;j++) {
+                if(j==kolom-1) {
+                    out.format("%d%n", m[i][j]);
+                } else {
+                    out.format("%d ", m[i][j]);
+                }
+            }
+        }
+        out.close();
+    }
 }
--- src/algeo/driver/Main.java
+++ src/algeo/driver/Main.java
@@ -1,7 +1,26 @@
 package algeo.driver;
 // disini isi input/output (semacam drivernya mungkin?) matriksnya (determinan dkk, kalau mau bikin class lgi sabi)
+import algeo.adt.Matriks;
+
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wow bisa!");
+        int[][] m, m2,m3;
+        try{
+            m = Matriks.bacaMatriks1("D:\\Tubes\\in.txt");
+            Matriks.tulisMatriks1(m,"D:\\Tubes\\out.txt");
+        } catch(FileNotFoundException exception) {
+            System.out.println("File tidak ditemukan");
+        }
+        try{
+            m2 = Matriks.bacaMatriks1("D:\\Tubes\\in2.txt");
+            Matriks.tulisMatriks1(m2,"D:\\Tubes\\out2.txt");
+        } catch(FileNotFoundException exception) {
+            System.out.println("File tidak ditemukan");
+        }
+        m3 = Matriks.bacaMatriks2();
+        Matriks.tulisMatriks2(m3);
     }
 }
