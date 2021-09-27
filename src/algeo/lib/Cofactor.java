99a78b27808158749f368f2038df4aad3c6d30c0
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2021-09-27 10:40:23 +0700

    Cofactor.java added

---
+++ src/algeo/lib/Cofactor.java
@@ -0,0 +1,18 @@
+package algeo.lib;
+
+import algeo.adt.Matriks;
+
+// Kelas kofaktor, berupa matriks
+public class Cofactor extends Matriks {
+    public Cofactor(Matriks matriks, int i, int j){
+        super(matriks.nRow()-1, matriks.nCol()-1);
+        for(int im = 0; im < matriks.nRow(); im++){
+            for(int jm = 0; jm < matriks.nCol(); jm++){
+                if (im == i || jm == j) continue;
+                int ic = (im < i) ? im : im - 1;
+                int jc = (jm < j) ? jm : jm - 1;
+                this.elmt[ic][jc] = matriks.elmt[im][jm];
+            }
+        }
+    }
+}
